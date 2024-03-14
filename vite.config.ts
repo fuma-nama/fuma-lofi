d6b3cda490ed55a263d57eb47941067aa97d7627
Fuma Nama <god63820869@gmail.com>
2024-03-14 18:50:45 +0800

    Migrate to ESM

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./"),
+      "@": path.dirname(fileURLToPath(import.meta.url)),
     },
   },
 });
